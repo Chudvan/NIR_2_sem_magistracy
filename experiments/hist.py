c6fdfa29c63f025ee4ed5df67c105b378a2e0e1b
ivan <Mr.Chudvan@yandex.ru>
2022-06-28 13:31:50 +0300

    instruments

---
+++ experiments/hist.py
@@ -0,0 +1,29 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import numpy as np
+
+app = Dash(__name__)
+
+
+app.layout = html.Div([
+    html.H4('Interactive normal distribution'),
+    dcc.Graph(id="graph"),
+    html.P("Mean:"),
+    dcc.Slider(id="mean", min=-3, max=3, value=0,
+               marks={-3: '-3', 3: '3'}),
+    html.P("Standard Deviation:"),
+    dcc.Slider(id="std", min=1, max=3, value=1,
+               marks={1: '1', 3: '3'}),
+])
+
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("mean", "value"),
+    Input("std", "value"))
+def display_color(mean, std):
+    data = np.random.normal(mean, std, size=500) # replace with your own data source
+    fig = px.histogram(data, range_x=[-10, 10])
+    return fig
+
+app.run_server(debug=True)
