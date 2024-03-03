3232d49ea8e31798453f2a2e50fbf2fbe26a3683
ivan <Mr.Chudvan@yandex.ru>
2024-03-03 13:45:32 +0300

    add joblib model '42 -> 7' + class ModelFACSEkmanOne + changes in global def load_models

--- model_interfaces.py
+++ model_interfaces.py
@@ -8,6 +8,7 @@ import sys
 import shutil
 import pandas as pd
 from tensorflow.keras.models import load_model
+from joblib import load
 import pickle
 
 
@@ -129,7 +130,7 @@ class AbstractModel(ABC):
         pass
 
 
-def load_models(self, path, neural=True, model_attrs=None):
+def load_models(self, path, ext='', model_attrs=None):
     dir_path = self.unzip_model(path)
     if model_attrs is None:
         model_attrs = self._get_model_attrs()
@@ -137,19 +138,23 @@ def load_models(self, path, neural=True, model_attrs=None):
         raise Exception(f'Число моделей < {len(model_attrs)}.')
     for model_attr in model_attrs:
         model_filename = model_attr[1:]
-        if not neural:
-            model_filename += '.pkl'
+        if ext:
+            model_filename += ext
         full_path = os.path.join(dir_path, model_filename)
         print(full_path)
         if not os.path.exists(full_path):
             shutil.rmtree(dir_path)
             raise Exception(f'Модель {model_filename} отсутствует в файле {self.filename}.')
         try:
-            if neural:
-                model_val = load_model(full_path)
-            else:
+            if ext=='.pkl':
                 with open(full_path, 'rb') as file:
                     model_val = pickle.load(file)
+            elif ext=='.joblib':
+                model_val = load(full_path)
+            elif not ext:
+                model_val = load_model(full_path)
+            else:
+                raise Exception(f'Неизвестное расширение {ext}.')
             setattr(self, model_attr, model_val)
             print(getattr(self, model_attr))
         except Exception:
@@ -212,7 +217,7 @@ class ModelVAClearStat(AbstractModel):
         return '2->7 (Stat)'
 
     def loadmodel(self, path):
-        load_models(self, path, neural=False)
+        load_models(self, path, '.pkl')
 
     def predict(self, df_VA):
         model_attrs = self._get_model_attrs()
@@ -254,7 +259,7 @@ class ModelClearVAStat(AbstractModel):
         return '7->2 (Stat)'
 
     def loadmodel(self, path):
-        load_models(self, path, neural=False)
+        load_models(self, path, '.pkl')
 
     def predict(self, df_seven):
         v = self._model_valence.predict(df_seven[df_seven.columns[:2]].values)[0][0] # N без [df_seven.columns[:2]] и мб без [0] для correct
@@ -364,7 +369,7 @@ class ModelFACSClearStat(AbstractModel):
         return '42->7 (Stat)'
 
     def loadmodel(self, path):
-        load_models(self, path, neural=False)
+        load_models(self, path, '.pkl')
 
     def predict(self, df_42):
         happy_vals = df_42[tools.happy_fields].values
@@ -386,6 +391,22 @@ class ModelFACSClearStat(AbstractModel):
         df_seven = pd.DataFrame([[neutral_res, happy_res, sad_res,
                                   angry_res, surprised_res, scared_res,
                                   disgusted_res]], columns=tools.seven_fields)
+        df_seven = tools.change_df_accuracy(df_seven)
+        return df_seven
+
+
+class ModelFACSEkmanOne(AbstractModel):
+    @classmethod
+    @property
+    def type_(cls):
+        return '42->7 (one)'
+
+    def loadmodel(self, path):
+        load_models(self, path, '.joblib')
+
+    def predict(self, df_42):
+        seven_vals = self._model.predict(df_42.values)
+        df_seven = pd.DataFrame(seven_vals, columns=tools.seven_fields)
         df_seven = tools.change_df_accuracy(df_seven)
         return df_seven
 
